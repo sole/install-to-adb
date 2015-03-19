1ea4643297af08c7f50ab253185023bf82154e12
sole <sole@mozilla.com>
2015-03-19 16:40:41 +0000

    actually launching the app

--- demo.js
+++ demo.js
@@ -1,8 +1,19 @@
 var path = require('path');
 var installToADB = require('./');
+var launchApp = require('node-firefox-launch-app');
 var appPath = path.join(__dirname, 'node_modules', 'sample-packaged-app');
+var Promise = require('es6-promise').Promise;
 
 installToADB(appPath).then(function(result) {
 	console.log('result', result);
-	process.exit(0);
+
+	Promise.all(result.map(function(res) {
+		return launchApp({
+			manifestURL: res.app.manifestURL,
+			client: res.client
+		});
+	})).then(function() {
+		process.exit(0);
+	});
+
 });
