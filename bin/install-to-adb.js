19659f4c6b00a24084412965f6e7ba1bd622601e
sole <sole@mozilla.com>
2015-04-01 13:38:20 +0100

    binary now can parse options

--- bin/install-to-adb.js
+++ bin/install-to-adb.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var nopt = require('nopt');
+var Promise = require('es6-promise').Promise;
 var installToADB = require('../');
-var appPath = process.argv[ process.argv.length - 1 ];
+var launchApp = require('node-firefox-launch-app');
+
+var opts = nopt({
+	launch: Boolean
+});
+
+var appPath = opts.argv.remain.join(' ');
 
 try {
 	appPath = fs.realpathSync(appPath);
@@ -13,7 +21,27 @@ try {
 
 console.log(appPath);
 installToADB(appPath).then(function(res) {
-	console.log('installed', res);
+	console.log('installed app');
+
+	// We were also asked to launch the app
+	if(opts.launch) {
+		
+		console.log('launching');
+		
+		var promises = res.map(function(r) {
+			return launchApp({
+				manifestURL: r.app.manifestURL,
+				client: r.client
+			});
+		});
+		
+		return Promise.all(promises);
+
+	} else {
+		return Promise.resolve(res);
+	}
+
+}).then(function() {
 	process.exit(0);
 });
 
